baf359cefa0fd686fd28e2b50088c29a2a52ce94
Jacob Gronert <jacobgronert@gmail.com>
2020-03-18 23:40:59 -0700

    minor changes

--- dance.sh
+++ dance.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
+lamp_ip='192.168.42.3'
 
-lamp_ip="$(tplight scan -t 1 | grep lamp | tr -s ' ' | cut -d ' ' -f 1)"
-
-if [ -z $lamp_ip ]; then 
-  echo "No lamp found."
-  exit 1
-else 
-  echo "Lamp found and ip is: $lamp_ip"
-fi
 
 while :
 do
-  lamp_ip="$(tplight scan -t 1 | grep lamp | tr -s ' ' | cut -d ' ' -f 1)"
   hue=$(( ( RANDOM % 360 )  + 1 ))
   saturation=100
   brightness=100
   echo "changing color to $hue"
-  tplight hsb -t 4000 $lamp_ip $hue $saturation $brightness
+  tplight hsb -t 3000 $lamp_ip $hue $saturation $brightness
   sleep 4
 done
