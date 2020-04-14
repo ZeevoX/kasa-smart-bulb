113e16ec62bf1b71d218e8c06a3dbd3d99670118
Timothy Langer <zeevox.dev@gmail.com>
2020-04-14 23:43:23 +0100

    Fix script permissions and convert to UNIX format
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- on.sh
+++ on.sh
@@ -1,9 +1,9 @@
 lamp_ip="$(tplight scan -t 1 | grep lamp | tr -s ' ' | cut -d ' ' -f 1)"
 
-if [ -z $lamp_ip ]; then 
+if [ -z $lamp_ip ]; then
   echo "No lamp found."
   exit 1
-else 
+else
   echo "Lamp found and ip is: $lamp_ip"
 fi
 
